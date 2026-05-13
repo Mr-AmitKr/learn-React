adb5683007a4bde7aaa4a0f2d855da13f285913d
Mr-Amitkr <im.amitkr7763@gmail.com>
2026-05-13 09:29:45 +0530

    push

--- learnReact/src/App.jsx
+++ learnReact/src/App.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import { FaBookmark } from "react-icons/fa";
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="parent">
+      <div className="card">
+        <div className="top">
+          <img
+            src="https://i.pinimg.com/736x/ff/a4/07/ffa407cb9ff38190e7ab88d15b9154d2.jpg"
+            alt=""
+          />
+          <button>
+            Save <FaBookmark size={16}  />
+          </button>
+        </div>
+        <div className="center">
+          <h3>Amazon <span>5 days Ago</span></h3>
+          <h2>Senior UI/UX Designer</h2>
+            <div className="tag">
+            <h4>Part Time</h4>
+            <h4>Senior Level</h4>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="plan">
+             <h3>$120/hr</h3>
+              <p>Mumbai,India</p>
+          </div> 
+          <div className="action">
+            <button>Apply now</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
